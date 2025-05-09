3091e97065723e3aabb83dc7a68f9b0ac7a2f0f4
eunju515 <130980334+eunju515@users.noreply.github.com>
2025-05-09 10:46:45 +0000

    api연습

---
+++ pages/page1.py
@@ -0,0 +1,2 @@
+import streamlit as st
+st.write("이것은 기본 텍스트 출력입니다.")
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,19 @@
 import streamlit as st
 st.write("이것은 기본 텍스트 출력입니다.")
+
+
+import openai
+
+user_api_key = st.text_input("키를 입력해주세요.")
+
+from openai import OpenAI
+
+client = OpenAI(api_key = user_api_key)
+prompt = st.text_input("프롬프트를 입력해주세요.")
+
+completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": prompt}]
+)
+st.markdown("### 💡 GPT의 답변:")
+st.write(completion.choices[0].message.content)
